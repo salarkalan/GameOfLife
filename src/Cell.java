30c60a88c264c208d6d13ce8b394e820fa9dbf6d
Salar Kalantari <145979810+salarkalan@users.noreply.github.com>
2024-06-05 16:26:39 +0200

    Added comments

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,9 @@
+/*
+ * The Cell Class represents each cell in the grid.
+ * It Ssores the current and next state of the cell (alive or dead).
+ */
+
+
 public class Cell {
     private boolean isAlive;
     private boolean nextState;
@@ -14,6 +20,7 @@ public class Cell {
         this.isAlive = isAlive;
     }
 
+    // The rules of the Game Of Life
     public void determineNextState(int aliveNeighbors) {
         if (isAlive) {
             nextState = aliveNeighbors == 2 || aliveNeighbors == 3;
